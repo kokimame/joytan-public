0925e98ebc84f98408c81b5e947c02a15ef2d22c
kokimame <kohkimametani@gmail.com>
2019-08-28 20:36:39 +0900

    simple play-pause button with emoji

--- public/js/addAudio.js
+++ public/js/addAudio.js
@@ -1,28 +1,27 @@
 function addAudioPlayer(wavRef, targetId, script) {
   wavRef.getMetadata().then((data) => {
-    var date = data["timeCreated"].slice(0, 10)
-    var path = data["fullPath"]
+    var date = data["timeCreated"].slice(0, 10);
+    var path = data["fullPath"];
+    var playerId = "".concat("player_", path);
+    var btnId = "".concat("btn_", path);
+
     wavRef.getDownloadURL().then((url) => {
       const div = document.createElement('div');
-      div.className = 'row';
+      div.className = 'playerTable';
       div.innerHTML = `
-      <table>
-        <col width="90px" />
+      <table class="table">
+      <audio id="${playerId}" src="${url}" type="audio/wav></audio>
+        <col width="40px" />
         <tr>
-          <td><font size="2">${date}</font></td>
+          <td style="width: 20%"><font size="2">${date}</font></td>
           <td>${script}</td>
         </tr>
         <tr>
-          <td></td>
-          <td>
-          <audio controls>
-            <source src="${url}" type="audio/wav">
-            Your browser does not support the audio element.
-          </audio>
-          </td>
-        </tr>
-        <tr>
-        <td></td>
+        <td>
+          <div>
+              <button id="${btnId}">▶️</button>
+          </div>
+        </td>
         <td>
           <form>
             <input type="radio" name="review" value="okay"> OK  
@@ -33,7 +32,25 @@ function addAudioPlayer(wavRef, targetId, script) {
       </tr>
       </table>
       `;
+ 
       document.getElementById(targetId).appendChild(div);
+ 
+      document.getElementById(playerId).addEventListener('ended', () => {
+        document.getElementById(btnId).innerText = '▶️'
+      })
+      
+      document.getElementById(btnId).addEventListener('click', () => {
+        var player = document.getElementById(playerId)
+        var button = document.getElementById(btnId)
+        console.log(player.paused)
+        if (player.paused) {
+          player.play()
+          button.innerText = '⏸️️'
+        } else if (!player.paused) {
+          player.pause()
+          button.innerText = '▶️'
+        }
+      }) 
     })
   })
 }
