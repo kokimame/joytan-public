3747be8bef8e3d44ac2087ef9e09910f02d61d94
kokimame <kokimametani@gmail.com>
2019-10-18 00:41:05 +0200

    test comment layout

--- public/js/addForum.js
+++ public/js/addForum.js
@@ -3,17 +3,32 @@ function addForum(entryData, index) {
     const toggleId = "toggle_" + index;
     const collapseId = "collapse_" + index;
     const boardId = "board_" + index;
+    const formId = "form_" + index;
     const numbering = (parseInt(index) + 1).toString();
 
     div.innerHTML = `
     <button class="btn btn-square btn-outline-dark btn-block text-left collapsed" type="button" 
     data-toggle="collapse" data-target="#${collapseId}" aria-expanded="false" id="${toggleId}">
-        <div class"btn-title" >
+        <div class="btn-title" >
             ${numbering}. ${entryData[wantedKey]}
         </div>
     </button>
     <div class="collapse" id="${collapseId}" data-parent="#forum-accordion">
       <div class="card card-body" id="${boardId}">
+        <div class="form-div">
+            <form class="form" id="${formId}">
+            <p class="name">
+                <input name="name" type="text" class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Name" id="name" />
+            </p>
+            <p class="text">
+                <textarea name="text" class="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Comment"></textarea>
+            </p>
+            <div class="submit">
+                <input type="submit" value="SEND" class="button-blue"/>
+                <div class="ease"></div>
+            </div>
+            </form>
+        </div>
     </div>
     `;
     // TODO: Audio auto-play
