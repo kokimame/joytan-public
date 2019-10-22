c841306a5cd44674cc680864c1c5772caf1e87c7
kokimame <kohkimametani@gmai.com>
2019-10-23 00:03:18 +0200

    Read/write comments

--- public/js/addForum.js
+++ public/js/addForum.js
@@ -3,9 +3,10 @@ function addForum(entryData, index) {
     const toggleId = "toggle_" + index;
     const collapseId = "collapse_" + index;
     const boardId = "board_" + index;
-    const formId = "form_" + index;
     const submitId = "submit_" + index;
+    const nameId = "name_" + index;
     const textId = "text_" + index;
+    const forumTarget = `forum/${dirname}/${index}`
     const numbering = (parseInt(index) + 1).toString();
     var upperNote = "";
     var lowerNote = "";
@@ -19,7 +20,6 @@ function addForum(entryData, index) {
     div.innerHTML = `
     <button class="btn btn-square btn-outline-dark btn-block text-left collapsed" type="button" 
     data-toggle="collapse" data-target="#${collapseId}" aria-expanded="false" id="${toggleId}">
-
         <div class="small-index">
             ${numbering}
         </div>
@@ -40,30 +40,76 @@ function addForum(entryData, index) {
         </table>
     </button>
     <div class="collapse" id="${collapseId}" data-parent="#forum-accordion">
-      <div class="card card-body" id="${boardId}">
-        <div class="comment">
-            <div class="commentor-name">
-            #1 Kohki Mametani
-            </div>
-            <div class="comment-text">
-            This is some comment written by an admin!
-            </div>
+      <div class="card card-body">
+        <div id="${boardId}" class="comment">
         </div>
-        <hr />
         <div class="form-div">
-            <span>Comment</span>
+            <div>
+                <div>Name</div>
+                <input id="${nameId}" type="text" name="usrname" placeholder="Name">
+            </div>
+            <div>Comment</div>
             <textarea id="${textId}" name="subject" 
             placeholder="Edit/Delete feature is yet to be introduced...
 Only off-topic comments/spams will be removed." style="height:150px"></textarea>
-            <button class="btn btn-success" id="${submitId}">Submit</button>
+            <button class="btn btn-success btn-submit" id="${submitId}">
+            Submit</button>
         </div>
     </div>
     `;
     // TODO: Audio auto-play
     document.getElementById('forum-accordion').appendChild(div);
 
-    document.getElementById(submitId).addEventListener("click", () => {
-        console.log($("#" + textId).val())
+    document.getElementById(toggleId).addEventListener("click", () => {
+        document.getElementById(boardId).innerHTML = ""
+        getComments(forumTarget, boardId)
     })
 
+    document.getElementById(submitId).addEventListener("click", () => {
+        var commentName = $("#" + nameId).val()
+        var commentText = $("#" + textId).val()
+        writeCommentData(forumTarget, commentName, commentText)
+    })
+}
+
+function getComments(reference, boardId) {
+    firebase.database().ref(reference).once('value').then(comments => {
+        var commentDatas = comments.val();
+        var cCounter = 0; 
+        for (var cRef in commentDatas){
+            cCounter += 1;
+            commentData = commentDatas[cRef];
+            var [cName, cText] = clientTextProc(
+                commentData["name"], commentData["text"]
+            );
+            console.log(commentData)
+            var commentHtml = `
+            <div class="comment">
+                <div class="commentor-name">
+                #${cCounter} ${cName}
+                </div>
+                <div class="comment-text">${cText}</div>
+                <hr class="comment-separator" />
+            </div>`
+            document.getElementById(boardId).innerHTML += commentHtml
+        }
+    })
+}
+
+function clientTextProc(name, text) {
+    if (name == "") {
+        name = "Anonymous writer"
+    }
+    return [name, text]
+}
+
+function writeCommentData(target, name, text) {
+    var commentData = {
+      text: text,
+      name: name
+    }
+    var newKey = firebase.database().ref(target).push().key;
+    var updates = {}
+    updates[newKey] = commentData
+    firebase.database().ref(target).update(updates)
 }
