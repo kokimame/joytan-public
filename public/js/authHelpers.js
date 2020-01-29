8334ef15d72e3f201d86d0ffcfe816f72debf84e
kokimame <code.kohkimametani@gmail.com>
2020-01-29 03:12:18 +0100

    changed order of auth provider

--- public/js/authHelpers.js
+++ public/js/authHelpers.js
@@ -38,8 +38,8 @@ function handleAuthButton() {
       signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
       ],
       // tosUrl and privacyPolicyUrl accept either url string or a callback
