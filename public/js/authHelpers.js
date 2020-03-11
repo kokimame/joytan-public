8ac8115f226345ba9e531ae8743e47e46698b59f
kokimame <code.kohkimametani@gmail.com>
2020-03-11 12:32:27 +0100

    Removed Facebook

--- public/js/authHelpers.js
+++ public/js/authHelpers.js
@@ -39,7 +39,7 @@ function handleAuthButton() {
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
       ],
       // tosUrl and privacyPolicyUrl accept either url string or a callback
